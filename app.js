b72fab5e8bfab51290b0bebbd43b15451ef9b8fe
César Augusto Macedo <cesar.macedo@sysmap.com.br>
2018-06-09 11:22:04 -0300

    first commit

--- app.js
+++ app.js
@@ -8,7 +8,26 @@ const assistant = new AssistantV1({
     //username: '2d8ed37a-ae2c-4f53-85c7-f946ef4310ae',
     url: 'https://gateway.watsonplatform.net/assistant/api',
     version: '2018-02-16',
-  });
+});
+
+const projectId = 'pizzaria-d6767'; //https://dialogflow.com/docs/agents#settings
+const sessionId = 'quickstart-session-id';
+const query = 'hello';
+const languageCode = 'pt-BR';
+
+const dialogflow = require('dialogflow');
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+        languageCode: languageCode,
+      },
+    },
+};
 
 const bot = new TelegramBot( TOKEN, { polling: true } )
 
@@ -17,15 +36,36 @@ bot.on( 'message', function ( msg ) {
     var id = msg.chat.id;
     var text = msg.text;
     logger.log('info', '[Chatbot] pergunta: ', text.toString());
-    const params = {
-        input: { "text":text },
-        //workspace_id: 'eaa92a38-ad1a-4bd4-af09-57ee9c67132d',
-        workspace_id:'80590cb1-58a7-4a3c-9d8c-6b364fa14059'
-    };
-    assistant.message(params, (err, response) => {
-        if (err)   bot.sendMessage(id,err);
-        
-        logger.log('info', '[Chatbot] resposta: ', response.output.text.toString());
-        bot.sendMessage(id, response.output.text.toString());
+    
+    sessionClient
+    .detectIntent(text)
+    .then(responses => {
+      console.log('Detected intent');
+      const result = responses[0].queryResult;
+      console.log(`  Query: ${result.queryText}`);
+      console.log(`  Response: ${result.fulfillmentText}`);
+      if (result.intent) {
+        console.log(`  Intent: ${result.intent.displayName}`);
+      } else {
+        console.log(`  No intent matched.`);
+      }
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
     });
+    
+    
+    
+    
+    // const params = {
+    //     input: { "text":text },
+    //     //workspace_id: 'eaa92a38-ad1a-4bd4-af09-57ee9c67132d',
+    //     workspace_id:'80590cb1-58a7-4a3c-9d8c-6b364fa14059'
+    // };
+    // assistant.message(params, (err, response) => {
+    //     if (err)   bot.sendMessage(id,err);
+        
+    //     logger.log('info', '[Chatbot] resposta: ', response.output.text.toString());
+    //     bot.sendMessage(id, response.output.text.toString());
+    // });
 }); 
